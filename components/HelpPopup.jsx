8a8d82aaf562e201a8963095efc1238179eded64
MinJunKim21 <jinju8012@gmail.com>
2022-09-08 16:28:03 +0900

    recoil 이용해서 popup modal 키고끄고 만들어둠

--- components/HelpPopup.jsx
+++ components/HelpPopup.jsx
@@ -1,22 +1,31 @@
 import { useState } from 'react';
 import { RiChatCheckLine } from 'react-icons/ri';
 import MessageModal from './MessageModal';
+import { modalState } from '../atoms/modalAtom';
+import { useRecoilState } from 'recoil';
 
 function HelpPopup() {
-  const [showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useRecoilState(modalState);
+
   const openModal = () => {
     setShowModal(true);
+    console.log('hi');
   };
 
   return (
     <div className="relative">
-      <button className="fixed right-0 bottom-0 " onClick={openModal}>
-        <RiChatCheckLine className="absolute right-0 bottom-0 h-14 w-14 bg-white text-gray-400  border-2 rounded-full m-3 p-3" />
+      <button
+        className="fixed right-0 bottom-0 cursor-pointer z-49 "
+        onClick={() => {
+          setShowModal(true);
+        }}
+      >
+        <RiChatCheckLine className="cursor-pointer absolute right-0 bottom-0 h-14 w-14 bg-white text-gray-400  border-2 rounded-full m-3 p-3" />
         <div className="absolute flex justify-center items-center font-extralight m-3 right-0 bottom-10 bg-red-500 w-5 h-5 rounded-full text-white text-xs text-center ">
           1
         </div>
       </button>
-      {openModal && <MessageModal />}
+      {showModal && <MessageModal />}
     </div>
   );
 }
--- components/MessageModal.jsx
+++ components/MessageModal.jsx
@@ -1,5 +1,40 @@
+import Image from 'next/image';
+import { GrClose } from 'react-icons/gr';
+import { modalState } from '../atoms/modalAtom';
+import { useRecoilState } from 'recoil';
+
 function MessageModal() {
-  return <div>MessageModal</div>;
+  const [showModal, setShowModal] = useRecoilState(modalState);
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
+  return (
+    <div open={showModal} onClose={handleClose}>
+      <div className="fixed top-0 z-50 bg-black bg-opacity-70 w-screen h-screen">
+        bg
+      </div>
+      <div className="bg-[#f2f2f2] w-50 h-50 fixed top-0 left-0 mt-12 z-50 w-full h-full rounded-t-[30px]">
+        <div className="px-4 pt-2">
+          <div className="flex items-center justify-between">
+            <Image
+              src="https://www.slid.cc/src/logo/slid_logo_with_text.png"
+              layout="fixed"
+              height={40}
+              width={80}
+              alt=""
+            />
+            <div onClick={handleClose}>
+              <i>
+                <GrClose />
+              </i>
+            </div>
+          </div>
+          <h5>Slid Customer Support</h5>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MessageModal;
