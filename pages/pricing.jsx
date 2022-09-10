e4ccb3ef36ee5a2a1a99f1f177678cc1063d599d
MinJunKim21 <jinju8012@gmail.com>
2022-09-11 02:00:36 +0900

    toggle switch 만드려는중

---
+++ pages/pricing.jsx
@@ -0,0 +1,107 @@
+import TopBar from '../components/TopBar';
+import { HiCheck } from 'react-icons/hi';
+import Image from 'next/image';
+
+function pricing() {
+  return (
+    <div className="max-w-xl px-4  pt-2">
+      <div className="flex justify-between">
+        <div className="">
+          <Image
+            src="https://www.slid.cc/src/logo/slid_logo_with_text.png"
+            layout="fixed"
+            height={40}
+            width={80}
+            alt=""
+          />
+        </div>
+        <button>
+          <span className="text-sm font-semibold border border-black rounded-lg px-3 py-2 hover:bg-gray-200">
+            내 노트함
+          </span>
+        </button>
+      </div>
+      <div className="pt-10">
+        <h1 className="text-center mx-auto text-2xl font-semibold">
+          슬리드와 함께 영상 속 지식을
+          <br />
+          캡처하러 가볼까요?
+        </h1>
+        <h5 className="text-[14px] font-light text-center mx-auto mt-4">
+          나에게 맞는 회원권을 선택해주세요.
+        </h5>
+      </div>
+
+      <div className="mt-4 bg-gray-200 rounded-md flex justify-around py-2 px-2">
+        <button className="bg-white flex w-full rounded-md py-1 ">
+          <span>12개월 회원권</span>
+        </button>
+        <button>
+          <span>1개월 회원권</span>
+        </button>
+      </div>
+
+      <div>
+        <h3>무료</h3>
+        <hr />
+        <h2>0원</h2>
+      </div>
+
+      <div>
+        <ul className="flex flex-col">
+          <li className="price-list">
+            <span>
+              <HiCheck />
+            </span>
+            <span>화면 분할된 동영상 노트</span>
+          </li>
+          <li className="price-list">
+            <span>
+              <HiCheck />
+            </span>
+            <span>매달 원클릭 캡처 200회</span>
+          </li>
+          <li className="price-list">
+            <span>
+              <HiCheck />
+            </span>
+            <span>캡처 속 텍스트 추출 200회</span>
+          </li>
+          <li className="price-list">
+            <span>
+              <HiCheck />
+            </span>
+            <span>캡처 위 펜 필기</span>
+          </li>
+          <li className="price-list">
+            <span>
+              <HiCheck />
+            </span>
+            <span>노트를 PDF / 워드로 변환</span>
+          </li>
+          <li className="price-list">
+            <span>
+              <HiCheck />
+            </span>
+            <span>캡쳐한 구간 영상 다시 보기</span>
+          </li>
+          <li className="text-[#828282] space-x-1 flex items-baseline">
+            <span>
+              <HiCheck />
+            </span>
+            <div>
+              매달 60초 영상 클립 녹화 20회
+              <br /> 브라우저(크롬, 웨일 등)에서만 가능해요.
+              <br />
+              데스크톱(Zoom 등)에서는 준비 중이에요!
+            </div>
+          </li>
+        </ul>
+      </div>
+
+      <button>현재 이용 중</button>
+    </div>
+  );
+}
+
+export default pricing;
--- pages/signup.jsx
+++ pages/signup.jsx
@@ -1,31 +1,24 @@
 import Image from 'next/image';
 import { FcGoogle } from 'react-icons/fc';
 import { AiFillApple } from 'react-icons/ai';
-import Link from 'next/link';
 
-function signin() {
+function signup() {
   return (
     <div className="flex flex-col justify-center mx-16 mt-20 max-w-xl ">
       <div className="justify-center mx-auto">
-        <Image
-          src="https://www.slid.cc/src/logo/slid_logo_with_text.png"
-          layout="fixed"
-          height={50}
-          width={100}
-          alt=""
-        />
+        <span className="font-bold text-2xl">슬리드 가입하기</span>
       </div>
       <button className="flex items-center justify-center space-x-3 text-lg font-semibold border border-black rounded-lg px-5 py-4  mt-8 ">
         <i className="text-xl">
           <FcGoogle />
         </i>
-        <span>구글 계정으로 로그인하기</span>
+        <span>구글 계정으로 가입하기</span>
       </button>
       <button className="flex items-center justify-center space-x-3 text-lg font-semibold border border-black rounded-lg px-5 py-4  mt-4 ">
         <i className="text-2xl">
           <AiFillApple />
         </i>
-        <span>애플 계정으로 로그인하기</span>
+        <span>애플 계정으로 가입하기</span>
       </button>
 
       <div className="flex mt-8 items-center space-x-2 ">
@@ -35,37 +28,52 @@ function signin() {
       </div>
 
       <form className="flex flex-col ">
+        <label className="mt-4">닉네임</label>
+        <input
+          type="text"
+          placeholder="Sliddy"
+          className="border-[1px] border-gray-300 rounded-md p-1 px-3 mt-2"
+        />
         <label className="mt-4">이메일</label>
         <input
           type="email"
           placeholder="hello@slid.cc"
           className="border-[2px] rounded-md p-1 px-3 mt-2"
         />
+        <span className="text-xs text-gray-500 mt-1">
+          해당 이메일로 확인 코드가 발송됩니다.
+        </span>
         <label className="mt-4">비밀번호</label>
         <input
           type="password"
+          placeholder=""
           className="border-[2px] rounded-md p-1 px-3 mt-2"
         />
+        <span className="text-xs text-gray-500 mt-1">
+          8자 이상의 비밀번호를 입력해주세요.
+        </span>
         <button
           type="submit"
           className="bg-blue-500 rounded-md text-lg font-semibold text-white mt-8 py-2 px-4"
         >
-          로그인
+          회원가입
         </button>
       </form>
+      <span className="text-xs text-gray-500 mt-8">
+        가입하기 버튼을 클릭하면 Slid의
+        <span className="text-[#2d85e9]"> 서비스 약관</span>과{' '}
+        <span className="text-[#2d85e9]">개인정보 수집 및 이용 동의서</span>에
+        동의하게 됩니다. 슬리드로부터 이메일을 받을 수 있으며 알림은 언제든지
+        수신 거부할 수 있습니다.
+      </span>
       <hr className="border-1 border-gray-600 flex flex-grow mt-4 opacity-30" />
 
-      <span className="mt-4 text-[#2d85e9]">비밀번호를 잊으셨나요?</span>
       <div className="mt-2 flex space-x-2">
-        <span>슬리드에 처음이신가요?</span>
-        <Link href="/signup">
-          <span className="text-[#2d85e9] hover:underline cursor-pointer ">
-            회원가입
-          </span>
-        </Link>
+        <span>이미 회원이신가요?</span>
+        <span className="text-[#2d85e9] mb-20">로그인</span>
       </div>
     </div>
   );
 }
 
-export default signin;
+export default signup;
